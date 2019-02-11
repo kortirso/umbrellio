48228f0a728640124c53dec40c1a043e5cf1c325
kortirso <kortirso@gmail.com>
2019-02-11 15:19:13 +0300

    fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,6 @@ class Post < ApplicationRecord
   def self.ips_with_users
     Post
       .joins(:user)
-      .where('posts.user_id IN (?)', User.pluck(:id))
       .group('posts.author_ip')
       .group('users.username')
       .order('posts.author_ip, users.username')
